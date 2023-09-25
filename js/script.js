af27ac820e6ca6fa7ba5e6eba1c7f02232d7b1b2
AgusHorvath94 <agushorvath994@gmail.com>
2023-09-25 12:48:11 -0300

    carrito compras

--- js/script.js
+++ js/script.js
@@ -22,9 +22,12 @@ fetch("magos.json")
         tituloMago.textContent = mago.name;
   
         // Creamos un icono de compra
-        const iconoCompra = document.createElement("img");
+        const iconoCompra = document.createElement("p");
         iconoCompra.classList.add("carrito");
-        iconoCompra.src = "compras.jpg"; // Cambia la ruta según la ubicación de tu imagen de carrito
+        const carritoTemplate = ` <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+      </svg>`;
+        iconoCompra.innerHTML = carritoTemplate 
   
         // Agregamos la imagen, el título y el icono de compra a la tarjeta
         magoCard.appendChild(imagenMago);
