a26feb3266881bf2b4f1ba6a71ab4bbf3bfba7bd
AgusHorvath94 <agushorvath994@gmail.com>
2023-10-04 00:38:15 -0300

    modifique cards de los magos

--- js/magos.js
+++ js/magos.js
@@ -21,27 +21,29 @@
                         //* Template  con los detalles del mago
                         
                         const magoTemplate = `
-                         <h1> ${magoElegido.name}</h1>
-                          <img src="${magoElegido.image}" alt="${magoElegido.name}">
-                          <p>Especie: ${magoElegido.species}</p>
-                          <p>Género: ${magoElegido.gender}</p>
-                          <p>Casa: ${magoElegido.house}</p>
-                          <p>Año de Nacimiento: ${magoElegido.yearOfBirth}
-                          <p>Es mago: ${magoElegido.wizard ? "Sí" : "No"}</p>
-                          <p>Linaje: ${magoElegido.ancestry}</p>
-                          <p>Color de Ojos: ${magoElegido.eyeColour}</p>
-                          <p>Color de Cabello: ${magoElegido.hairColour}</p>
-                          <p>Varita: Madera - ${magoElegido.wand.wood},  
-                          <p>Longitud - ${magoElegido.wand.length} pulgadas</p>
-                          <p>Patronus: ${magoElegido.patronus}</p>
-                          <p>Estudiante en Hogwarts: ${magoElegido.hogwartsStudent ? "Sí" : "No"}</p>
-                          <p>Personal de Hogwarts: ${magoElegido.hogwartsStaff ? "Sí" : "No"}</p>
-                          <p>Actor: ${magoElegido.actor}</p>
-                          <p>¿Está vivo?: ${magoElegido.alive ? "Sí" : "No"}</p>
+                         <h1> ${magoElegido.name}</h1><br><br>
+                         <div class="container">
+                         <div class="mago-img">
+                         <img src="${magoElegido.image}" alt="${magoElegido.name}">
+                         </div>                         
+                         <div class="mago-desc">
+                         <p>Especie: ${magoElegido.species}</p>
+                         <p>Género: ${magoElegido.gender}</p>
+                         <p>Casa: ${magoElegido.house}</p>
+                         <p>Año de Nacimiento: ${magoElegido.yearOfBirth}
+                         <p>Es mago: ${magoElegido.wizard ? "Sí" : "No"}</p>
+                         <p>Linaje: ${magoElegido.ancestry}</p>
+                         <p>Color de Ojos: ${magoElegido.eyeColour}</p>
+                         <p>Color de Cabello: ${magoElegido.hairColour}</p>
+                         <p>Varita: Madera - ${magoElegido.wand.wood},  
+                         <p>Longitud - ${magoElegido.wand.length} pulgadas</p>
+                         <p>Patronus: ${magoElegido.patronus}</p>
+                         <p>Estudiante en Hogwarts: ${magoElegido.hogwartsStudent ? "Sí" : "No"}</p>
+                         <p>Personal de Hogwarts: ${magoElegido.hogwartsStaff ? "Sí" : "No"}</p>
+                         <p>Actor: ${magoElegido.actor}</p>
+                         <p>¿Está vivo?: ${magoElegido.alive ? "Sí" : "No"}</p>
+                         </div>
                           
-                          
-                        
-                          <button><a href="index.html">VOLVER A LA PAGINA PRINCIPAL</a></button>
                         `;
                   
                        
